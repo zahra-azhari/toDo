c2832b8e11055b42b5e7d5920e57d109c1e07f12
Azhari <z.azharirad@gmail.com>
2025-07-12 11:44:19 +0430

    forth edit

--- A/home/urls.py
+++ A/home/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('',views.home),
-    path('hello/',views.say_hello),
-    path('detail/<int:todo_id>/',views.detail)
+    path('', views.home),
+    path('hello/', views.say_hello),
+    path('detail/<int:todo_id>/', views.detail)
 ]
-
