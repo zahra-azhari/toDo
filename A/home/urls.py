947dda7fb8a279b7f4f305914f5dabf22bc67948
Azhari <z.azharirad@gmail.com>
2025-07-15 11:39:36 +0430

    fifth edit

--- A/home/urls.py
+++ A/home/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('hello/', views.say_hello),
-    path('detail/<int:todo_id>/', views.detail)
+    path('', views.home,name='home'),
+    path('details/<int:todo_id>/', views.detail , name='details'),
+    path('delete/<int:todo_id>/', views.delete, name='delete'),
+
 ]
--- A/home/views.py
+++ A/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Todo
 # from django.http import HttpResponse
 # Create your views here.
@@ -8,11 +8,12 @@ def home (request):
     all=Todo.objects.all()
     return render(request,'home.html',{'all':all})
 
-def say_hello(request):
-    # person ={'name':'zahra'}
-    return render(request,'hello.html',{'name':'adminn'})
 
 
 def detail(request,todo_id):
     todo=Todo.objects.get(id=todo_id)
     return render(request, 'detail.html',{'todo':todo})
+
+def delete(request,todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    return redirect('home')
