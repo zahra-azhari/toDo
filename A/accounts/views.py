d7885ddbc03612280f1f9a74e384f7b00d835008
Azhari <z.azharirad@gmail.com>
2025-08-02 16:29:32 +0430

    tenth edit

--- A/accounts/views.py
+++ A/accounts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import UserRegistrationForm
 
 # Create your views here.
 
 
 
 def user_register(request):
-    return HttpResponse('User registeration page')
+    if request.method == 'POST':
+        form=UserRegistrationForm(request.POST)
+        if form.is_valid():
+            pass
+    else:
+        form=UserRegistrationForm()
+    return render(request, 'register.html', {'form':form})
